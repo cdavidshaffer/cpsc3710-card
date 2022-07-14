d8f65125d59823d11843910c6b7dafd14f5a4ca1
C. David Shaffer <cdshaffer@acm.org>
2022-07-14 12:39:32 -0400

    Post enum lecture

--- src/main/java/edu/au/cpsc/carddemo/model/Card.java
+++ src/main/java/edu/au/cpsc/carddemo/model/Card.java
@@ -7,48 +7,56 @@ package edu.au.cpsc.carddemo.model;
 /**
  * I am a playing card from a standard 52 card deck.
  *
- * <p>A standard deck contains 52 unique cards differentiated by their face and suit.</p>
+ * <p>A standard deck contains 52 unique cards differentiated by their rank and suit.</p>
  *
- * <p>Cards are created using the two-argument constructor: {@link Card(int, int)}.</p>
+ * <p>Cards are created using the two-argument constructor: {@link #Card(Rank, Suit)}.</p>
  *
  * <p>Example:</p>
  * <pre>Card c = new Card(Card.ACE, Card.DIAMONDS);</pre>
  */
 public class Card {
 
-  public static final int HEARTS = 0;
-  public static final int DIAMONDS = 1;
-  public static final int SPADES = 2;
-  public static final int CLUBS = 3;
-  public static final int ACE = 0;
-  public static final int TWO = 2;
-  public static final int THREE = 2;
-  public static final int FOUR = 4;
-  public static final int FIVE = 5;
-  public static final int SIX = 6;
-  public static final int SEVEN = 7;
-  public static final int EIGHT = 8;
-  public static final int NINE = 9;
-  public static final int TEN = 10;
-  public static final int JACK = 11;
-  public static final int QUEEN = 12;
-  public static final int KING = 13;
+  private Rank rank;
+  private Suit suit;
 
-  private int face;
-  private int suit;
-
-  public Card(int face, int suit) {
-    this.face = face;
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
     this.suit = suit;
   }
 
-  public int getFace() {
-    return face;
+  public Rank getRank() {
+    return rank;
   }
 
-  public int getSuit() {
+  public Suit getSuit() {
     return suit;
   }
 
+  @Override
+  public String toString() {
+    return getRankString() + " of " + getSuitString();
+  }
+
+  private String getSuitString() {
+    return suit.name().toLowerCase();
+  }
+
+  private String getRankString() {
+    return rank.name().toLowerCase();
+  }
+
+  /**
+   * The suit of a card.
+   */
+  public enum Suit {
+    HEARTS, DIAMONDS, SPADES, CLUBS
+  }
+
+  /**
+   * The rank of a card.
+   */
+  public enum Rank {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+  }
 
 }
