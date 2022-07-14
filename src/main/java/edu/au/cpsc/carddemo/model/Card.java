27c4ef0b04bb171b47f14deb31be6b91c2bc8f59
C. David Shaffer <cdshaffer@acm.org>
2022-07-14 12:18:38 -0400

    Pre enum lecture

--- src/main/java/edu/au/cpsc/carddemo/model/Card.java
+++ src/main/java/edu/au/cpsc/carddemo/model/Card.java
@@ -1,79 +1,54 @@
 /*
- * Copyright (c) 2022. See LICENSE.txt for details.
+ * Copyright 2022.  C. David Shaffer.  See LICENSE.txt for complete copyright.
  */
 
 package edu.au.cpsc.carddemo.model;
 
-import java.io.IOException;
-
 /**
  * I am a playing card from a standard 52 card deck.
  *
- * <p>A standard deck contains 52 unique cards differentiated by their face and suit.  See Suit and
- * Face classes for possible values.</p>
+ * <p>A standard deck contains 52 unique cards differentiated by their face and suit.</p>
  *
- * <p>Cards are created using the two-argument constructor: {@code Card(Face, Suit)}.</p>
+ * <p>Cards are created using the two-argument constructor: {@link Card(int, int)}.</p>
  *
  * <p>Example:</p>
- * <pre>Card c = new Card(Card.Face.ACE, Card.Suit.DIAMONDS);</pre>
- * <p>
+ * <pre>Card c = new Card(Card.ACE, Card.DIAMONDS);</pre>
  */
 public class Card {
 
-  private Face face;
-  private Suit suit;
+  public static final int HEARTS = 0;
+  public static final int DIAMONDS = 1;
+  public static final int SPADES = 2;
+  public static final int CLUBS = 3;
+  public static final int ACE = 0;
+  public static final int TWO = 2;
+  public static final int THREE = 2;
+  public static final int FOUR = 4;
+  public static final int FIVE = 5;
+  public static final int SIX = 6;
+  public static final int SEVEN = 7;
+  public static final int EIGHT = 8;
+  public static final int NINE = 9;
+  public static final int TEN = 10;
+  public static final int JACK = 11;
+  public static final int QUEEN = 12;
+  public static final int KING = 13;
 
-  public Card(Face face, Suit suit) {
+  private int face;
+  private int suit;
+
+  public Card(int face, int suit) {
     this.face = face;
     this.suit = suit;
   }
 
-  public Face getFace() {
+  public int getFace() {
     return face;
   }
 
-  public Suit getSuit() {
+  public int getSuit() {
     return suit;
   }
 
-  /**
-   * The score value of this card in hodge-podge.
-   *
-   * @param foo the annotation for the card
-   * @return the hodge-podge score
-   * @throws IllegalArgumentException when foo is illegal
-   * @deprecated will be removed in v2.0, call {@link #getValue(int)} instead
-   */
-  @Deprecated
-  public int value(int foo) throws IOException {
-    return getValue(foo);
-  }
 
-  /**
-   * The score value of this card in hodge-podge.
-   *
-   * @param foo the annotation for the card
-   * @return the hodge-podge score
-   * @throws IllegalArgumentException when foo is illegal
-   */
-  public int getValue(int foo) {
-    if (face == Face.ACE || (suit == Suit.HEARTS && face == Face.THREE)) {
-      return 5;
-    } else if (face == Face.TWO || ((suit == Suit.HEARTS || suit == Suit.DIAMONDS)
-        || suit == Suit.DIAMONDS && face == Face.TEN)) {
-      // TODO not sure!
-      return 9;
-    } else {
-      return 22;
-    }
-  }
-
-  public enum Face {
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
-  }
-
-  public enum Suit {
-    HEARTS, DIAMONDS, CLUBS,
-    SPADES;
-  }
 }
